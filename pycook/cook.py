d0af5062b8136052e55277020e3e09e52e8df50e
Oleh Krehel <ohwoeowho@gmail.com>
2017-10-17 18:12:04 +0200

    Allow recipe customization via ~/.cookrc.py
    Example file content:
    
        config = {
            "/project/Cookbook.py": {
                "tee": {
                    "recipes": "*",
                    "location": "/log/cook"
                }
            }
        }
    
    This will result the output of all recipes from "/project/Cookbook.py"
    being saved to "/log/cook/..." in addition to being echoed to stdout.
    
    Example file name:
        /log/cook/<2017-10-13_Fri_17:44>-git_p4_shelve.txt
    
    The file name incorporates the timestamp of the job start and the
    recipe name.

--- pycook/cook.py
+++ pycook/cook.py
@@ -1,6 +1,7 @@
 #* Includes
 import sys
 import os
+import subprocess
 import imp
 import inspect
 from pycook import elisp as el
@@ -14,6 +15,16 @@ def recipe_dict(book):
     mod = imp.load_source("Cookbook", book)
     funs = inspect.getmembers(mod, inspect.isfunction)
     return dict(el.cl_remove_if_not(recipe_p, funs))
+
+def book_config(book):
+    rc_file = el.expand_file_name("~/.cookrc.py")
+    if el.file_exists_p(rc_file):
+        mod = imp.load_source("book_config", rc_file)
+        config = mod.config
+        if book in config:
+            cfg = config[book]
+            return cfg
+    return {}
 
 def recipe_names(book):
     return "\n".join(recipe_dict(book).keys())
@@ -29,6 +40,11 @@ def get_book():
     book = os.path.realpath(caller_file)
     return book
 
+def log_file_name(basedir, recipe):
+    ts = el.replace_regexp_in_string(" ", "_", el.timestamp())
+    name = el.lf("{ts}-{recipe}.txt")
+    return el.expand_file_name(name, basedir)
+
 def main(argv = None, book = None):
     if argv is None:
         argv = sys.argv
@@ -38,7 +54,15 @@ def main(argv = None, book = None):
         if argv[1] == "--list":
             print(recipe_names(book))
         else:
-            fun = recipe_dict(book)[argv[1]]
-            run(fun(42))
+            recipe = argv[1]
+            fun = recipe_dict(book)[recipe]
+            cfg = book_config(book)
+            if "tee" in cfg:
+                basedir = cfg["tee"]["location"]
+                fname = log_file_name(basedir, recipe)
+                tee = subprocess.Popen(["tee", fname], stdin = subprocess.PIPE)
+                os.dup2(tee.stdin.fileno(), sys.stdout.fileno())
+                os.dup2(tee.stdin.fileno(), sys.stderr.fileno())
+            run(fun(42), echo = True)
     else:
         print(describe(book))
