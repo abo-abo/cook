d015f43d7276e202b9bf4f952bdc5d63f83f4544
Oleh Krehel <ohwoeowho@gmail.com>
2017-12-07 18:24:32 +0100

    Introduce reusable recipes
    * Cookbook.py: import recipes like this:
    
        from pycook.recipes.pip import clean, sdist

--- Cookbook.py
+++ Cookbook.py
@@ -1,24 +1,14 @@
 #* Imports
-from pycook.elisp import sc
-
-#* Functions
-def pip3_package_installed_p(package):
-    s = sc("pip3 show {package}")
-    return s != ""
+import pycook.recipes.pip as pip
+from pycook.recipes.pip import clean, sdist
 
 #* Recipes
 def pip_reinstall(recipe):
     res = []
-    if pip3_package_installed_p("pycook"):
-        res.append("sudo -H pip3 uninstall -y pycook")
-    res.append("sudo -H pip3 install .")
+    if pip.package_installed_p("pycook"):
+        res += [pip.uninstall("pycook")]
+    res += [pip.install(".")]
     return res
 
 def publish(recipe):
-    return ["rm -rf dist/",
-            "python3 setup.py sdist",
-            "twine upload dist/*"]
-
-def clean(recipe):
-    return ["rm -rf dist",
-            "rm -rf pycook.egg-info"]
+    return sdist(recipe) + ["twine upload dist/*"]
---
+++ pycook/recipes/pip.py
@@ -0,0 +1,39 @@
+#* Imports
+import sys
+import shutil
+from pycook.elisp import sc
+
+#* Functions
+def get_python():
+    return (shutil.which("python3") or
+            shutil.which("python"))
+
+def get_pip():
+    if sys.version_info.major == 3 and shutil.which("pip3"):
+        return "pip3"
+    else:
+        return "pip"
+
+def package_installed_p(package):
+    try:
+        s = sc(get_pip() + " show " + package)
+        return s != ""
+    except:
+        return False
+
+def uninstall(package):
+    return "sudo -H " + get_pip() + " uninstall -y " + package
+
+def install(package):
+    return "sudo -H " + get_pip() + " install " + package
+
+#* Recipes
+def sdist(recipe):
+    return [
+        "rm -rf dist/",
+        get_python() + " setup.py sdist"]
+
+def clean(recipe):
+    return [
+        "rm -rf dist",
+        "rm -rf *.egg-info"]
