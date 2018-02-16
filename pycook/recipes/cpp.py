fd0a1813bcf071fa5e4649517c55349c5da5cee5
Oleh Krehel <ohwoeowho@gmail.com>
2018-02-16 18:15:58 +0100

    pycook/recipes/cpp.py (compile_and_run): Update

--- pycook/recipes/cpp.py
+++ pycook/recipes/cpp.py
@@ -6,11 +6,14 @@ lf = el.lf
 
 #* Functions
 def compile_and_run(source_file):
+    sources = source_file
+    if type(source_file) is list:
+        source_file = source_file[0]
     assert(el.file_exists_p(source_file))
     exe_file = re.sub("cc$", "e", source_file)
     res = []
     if (not el.file_exists_p(exe_file) or
-        el.file_newer_than_file_p(source_file, exe_file)):
-        res += [lf("g++ -g -O2 -std=c++11 -o {exe_file} {source_file}")]
+        any([el.file_newer_than_file_p(f, exe_file) for f in sources])):
+        res += [lf("g++ -g -O2 -std=c++11 -o {exe_file} ") + " ".join(sources)]
     res += [lf("./{exe_file}")]
     return res
