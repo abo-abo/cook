61be2ace08b24bc6c644946b3482bfae9228c267
Oleh Krehel <ohwoeowho@gmail.com>
2018-03-01 20:19:38 +0100

    pycook/recipes/cpp.py (compile_and_run): Re-run rule on Cookbook.py change

--- pycook/recipes/cpp.py
+++ pycook/recipes/cpp.py
@@ -2,6 +2,7 @@
 import sys
 import re
 import pycook.elisp as el
+import pycook.cook as cook
 lf = el.lf
 
 #* Functions
@@ -24,7 +25,7 @@ def compile_and_run(inputs, flags = ""):
     exe_file = re.sub("cc$", "e", main_file)
     res = []
     if (not el.file_exists_p(exe_file) or
-        any([el.file_newer_than_file_p(f, exe_file) for f in sources])):
+        any([el.file_newer_than_file_p(f, exe_file) for f in sources + [cook.script_get_book()[0]]])):
         res += [lf("g++ -g -O2 -std=c++11 {flags} -o {exe_file} ") + " ".join(sources) + libs_str]
     res += [lf("./{exe_file}")]
     return res
