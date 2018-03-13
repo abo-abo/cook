0c1afe697608dbe0067fe10c0d89fd55e4094601
Oleh Krehel <ohwoeowho@gmail.com>
2018-03-13 18:04:46 +0100

    pycook/recipes/cpp.py (compile_and_run): Add idirs and libs args
    Libraries can now be specified using the full file path.  In this
    case, LD_LIBRARY_PATH will be automatically set up for the execution.

--- pycook/recipes/cpp.py
+++ pycook/recipes/cpp.py
@@ -6,7 +6,10 @@ import pycook.cook as cook
 lf = el.lf
 
 #* Functions
-def compile_and_run(inputs, std = "c++11", flags = ""):
+def lib_name(fname):
+    return "-l:" + el.file_name_nondirectory(fname)
+
+def compile_and_run(inputs, std = "c++11", flags = "", idirs = [], libs = []):
     """Compile INPUTS together into an executable and run it.
 
     INPUTS is a list of source files, headers and libraries.
@@ -20,12 +23,18 @@ def compile_and_run(inputs, std = "c++11", flags = ""):
     assert(el.file_exists_p(main_file))
     sources = el.re_filter("(cc|cpp|h|hh|hpp)$", inputs)
     diff = set(inputs) - set(sources)
-    libs = [x for x in inputs if x in diff]
-    libs_str = " " + " ".join(["-l" + lib for lib in libs])
+    libs_inl = [x for x in inputs if x in diff]
+    libs_str = " " + " ".join(["-l" + lib for lib in libs_inl])
+    lib_dirs = set(el.mapcar(el.file_name_directory, libs))
+    lflags = el.mapcar(lib_name, libs)
+    libs_str += " " + " ".join(["-L" + d for d in lib_dirs] + lflags)
     exe_file = re.sub("cc$", "e", main_file)
     res = []
+    flags += " " + " ".join(["-I" + d for d in idirs])
     if (not el.file_exists_p(exe_file) or
         any([el.file_newer_than_file_p(f, exe_file) for f in sources + [cook.script_get_book()[0]]])):
         res += [lf("g++ -g -O2 -std={std} {flags} -o {exe_file} ") + " ".join(sources) + libs_str]
+    if lib_dirs:
+        res += ["export LD_LIBRARY_PATH=" + ":".join(lib_dirs)]
     res += [lf("./{exe_file}")]
     return res
