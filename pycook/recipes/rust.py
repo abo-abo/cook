1071474fb367a314242ad4adbf8973414c805ddc
Oleh Krehel <ohwoeowho@gmail.com>
2018-12-06 12:01:38 +0100

    pycook/recipes/rust.py: Add

---
+++ pycook/recipes/rust.py
@@ -0,0 +1,15 @@
+#* Imports
+import pycook.elisp as el
+lf = el.lf
+
+#* Functions
+def compile_and_run(src_file):
+    src_file = "main.rs"
+    exe_file = el.file_name_sans_extension(src_file)
+    if (el.file_exists_p(exe_file) and
+        el.file_newer_than_file_p(exe_file, src_file)):
+        return [lf("./{exe_file}")]
+    else:
+        return [
+            lf("rustc {src_file}"),
+            lf("./{exe_file}")]
