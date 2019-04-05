32b9e06fbc202e4138c5a83388edc443e1256624
Oleh Krehel <ohwoeowho@gmail.com>
2019-04-05 19:25:22 +0200

    pycook/recipes/dpkg.py (ls): Add manual completion
    When `recipe' is an integer:
    
    - return the list of commands.
    
    When `recipe' is a two-item tuple:
    
    - make sure the first element is "complete"
    - extract the part already typed in from the second element
    - return a string with completions separated by newlines

--- pycook/recipes/dpkg.py
+++ pycook/recipes/dpkg.py
@@ -1,5 +1,14 @@
+import pycook.elisp as el
+
 def ls(recipe, package):
-    return ["dpkg -L " + package]
+    if type(recipe) is int:
+        return ["dpkg -L " + package]
+    elif recipe[0] == "complete":
+        part = recipe[1]
+        if len(part) > 0:
+            part += "*"
+        return el.shell_command_to_string("dpkg-query -W -f '${Package}\n' " + part)
+
 
 def file_to_package(recipe, fname):
     return ["dpkg -S " + fname]
