42540adfb8203996957354bba4d68d6fe47c01ec
Oleh Krehel <ohwoeowho@gmail.com>
2019-06-27 11:44:01 +0200

    pycook/recipes/org.py (export_pdf): Rename arg to fname so we get free completion

--- pycook/recipes/org.py
+++ pycook/recipes/org.py
@@ -11,10 +11,10 @@ def emacs_eval(expr):
     e = re.sub('"', "\\\"", expr)
     return lf('emacs -batch -l /usr/local/cook/scripts.el --eval "{e}"')
 
-def export_pdf(recipe, forg):
-    fpdf = re.sub("org$", "pdf", forg)
+def export_pdf(recipe, fname):
+    fpdf = re.sub("org$", "pdf", fname)
     return [
-        emacs_eval(lf('(org-to-pdf "{forg}")')),
+        emacs_eval(lf('(org-to-pdf "{fname}")')),
         lf("nohup 1>/dev/null 2>/dev/null evince {fpdf} &"),
         el.emacsclient_eval('(with-current-buffer (window-buffer (selected-window)) (bury-buffer))')
     ]
