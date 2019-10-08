637e9b7217520a3c070f4d6f21d4fb4a73a632b5
Oleh Krehel <ohwoeowho@gmail.com>
2019-10-08 11:16:42 +0200

    pycook/recipes/emacs.py: Select directory files if no fnames

--- pycook/recipes/emacs.py
+++ pycook/recipes/emacs.py
@@ -8,5 +8,7 @@ def install(recipe, *packages):
     return el.emacs_batch_eval(lf("(cook-install-deps '({packages_str}))"))
 
 def compile(recipe, *fnames):
+    if not fnames:
+        fnames = el.directory_files(el.default_directory(), False, ".*el$")
     fnames_str = " ".join(['"' + fname + '"' for fname in fnames])
     return el.emacs_batch_eval(lf("(cook-byte-compile {fnames_str})"))
