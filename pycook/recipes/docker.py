e5c40b57a4125b8bf79440b033ce0983c779d11c
Oleh Krehel <ohwoeowho@gmail.com>
2019-11-05 18:01:24 +0100

    pycook/recipes/docker.py: Move example here

--- pycook/recipes/docker.py
+++ pycook/recipes/docker.py
@@ -1,6 +1,36 @@
+def list_all_containers(recipe):
+    return ["docker ps"]
+
+def list_all_images(recipe):
+    return ["docker images"]
+
+def stop_all_containers(recipe):
+    return ["docker stop $(docker ps -aq)"]
+
+def remove_stopped_containers(recipe):
+    return ["docker rm $(docker ps -aq)"]
+
+def remove_untagged_images(recipe):
+    return ["docker rmi $(docker images | grep '^<none>' | awk '{print $3}')"]
+
 def force_service_restart(recipe):
     """Useful when Docker stops working after upgrading kernel."""
     return [
         "sudo rm -rf /var/lib/docker",
         "sudo service docker restart"
     ]
+
+def setup_install_docker(recipe):
+    return [
+        "sudo apt-get update",
+        "sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual",
+        "sudo apt-get update",
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+        "sudo apt-get install docker-ce",
+        "docker --version"]
+
+def setup_run_docker_without_sudo(recipe):
+    return [
+        "sudo groupadd docker || echo 'ignore'",
+        "sudo gpasswd -a $USER docker",
+        "echo 'Log out/in to make results premanent'"]
