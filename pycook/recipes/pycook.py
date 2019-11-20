c6760833b91f60f26d8f2fc96cea48d3a4549e26
Oleh Krehel <ohwoeowho@gmail.com>
2019-11-20 15:38:12 +0100

    pycook/recipes/pycook.py (setup_dotfiles): Add recipe

--- pycook/recipes/pycook.py
+++ pycook/recipes/pycook.py
@@ -1,6 +1,7 @@
 #* Imports
 import os
 import pycook.elisp as el
+import pycook.insta as st
 
 #* Recipes
 def update(recipe):
@@ -8,6 +9,14 @@ def update(recipe):
     cook_el = el.emacs_cook_script("cook.el")
     if "INSIDE_EMACS" in os.environ:
         el.sc(el.emacsclient_eval(el.lf('(load-file "{cook_el}")')))
+
+def setup_dotfiles(recipe):
+    st.patch("~/.profile", ['+PATH="$HOME/.local/bin:$PATH"'])
+    st.patch("~/.bashrc", ["""
++if [ -f ~/.local/cook/bash-completion.sh ]; then
++    . ~/.local/cook/bash-completion.sh
++fi
+"""])
 
 def profile(recipe):
     return "pyinstrument $(which cook) :linux ls_users"
