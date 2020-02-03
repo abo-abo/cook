e870f0c6fc2a14b61591fe058cb73d74a0e3b73e
Oleh Krehel <ohwoeowho@gmail.com>
2020-02-03 11:51:22 +0100

    pycook/recipes/python.py (http_server): Update
    Since cook.el doesn't do "setsid -w" anymore.

--- pycook/recipes/python.py
+++ pycook/recipes/python.py
@@ -1,5 +1,7 @@
 def http_server(recipe):
     return [
         "python -m SimpleHTTPServer &",
-        "xdg-open http://localhost:8000"
+        "python_process=$!",
+        "sleep 0.5 && xdg-open http://localhost:8000",
+        "wait $python_process",
     ]
