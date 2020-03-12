4c583cf17fa0f36a981ba2001fb86c2640f1ab14
Oleh Krehel <ohwoeowho@gmail.com>
2020-03-12 14:06:44 +0100

    pycook/insta.py (chmod): Work with HOST

--- pycook/insta.py
+++ pycook/insta.py
@@ -98,26 +98,17 @@ def cp(fr, to):
         return True
 
 def chmod(fname, permissions):
-    fparts = parse_fname(fname)
-    if isinstance(fparts, list):
-        (host, f) = fparts
-        current = sc("ssh {host} stat -c %a {f}")
-        if current == permissions:
-            print(lf("{fname}: OK"))
-            return False
-        else:
-            cmd = lf("ssh {host} chmod {permissions} {f}")
-            bash(cmd)
-            return True
+    current = sc("stat -c %a {fname}")
+    if current == permissions:
+        print(lf("{fname}: OK"))
+        return False
     else:
-        current = sc("stat -c '%a' {fname}")
-        if current == permissions:
-            print(lf("{fname}: OK"))
-            return False
+        if el.HOST is not None:
+            scb("chmod {permissions} {fname}")
         else:
             cmd = sudo(lf("chmod {permissions} {fname}"), fname)
             bash(cmd)
-            return True
+        return True
 
 def chown(fname, owner):
     fparts = parse_fname(fname)
