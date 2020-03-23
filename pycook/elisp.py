28290ef298d45bd45da7c66e65176a18616119fd
Oleh Krehel <ohwoeowho@gmail.com>
2020-03-23 14:23:20 +0100

    pycook/elisp.py (parse_fname): "./" means it's on localhost

--- pycook/elisp.py
+++ pycook/elisp.py
@@ -163,6 +163,8 @@ def file_name_nondirectory(f):
 def parse_fname(fname):
     if not isinstance(fname, str):
         return fname
+    elif fname[0] == ".":
+        return (None, os.path.realpath(expand_file_name(fname)))
     elif ":" in fname:
         return fname.split(":")
     elif HOST is not None:
