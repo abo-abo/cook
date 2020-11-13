77b05238ae4cf9dbf4c2fc9f5927c45fef54e8f2
Oleh Krehel <ohwoeowho@gmail.com>
2020-11-13 20:27:00 +0100

    etc/cook: Add -u for unbuffered output

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycook',
-    version='0.9.14',
+    version='0.9.15',
     description='Makefile -> Cookbook.py.',
     long_description='https://raw.githubusercontent.com/abo-abo/cook/master/README.org',
     packages=find_packages(exclude=['etc', 'examples']),
