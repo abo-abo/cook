e07bfdcdc7d6a67cad9d583371f5e724d24ea8d2
Oleh Krehel <ohwoeowho@gmail.com>
2021-10-18 22:30:16 +0200

    pycook/recipes/pdf.py (concat): Add

--- pycook/recipes/pdf.py
+++ pycook/recipes/pdf.py
@@ -1,3 +1,6 @@
 def extract_page(recipe, fname, page):
     (base, ext) = fname.split(".")
     return f"pdftk {fname} cat {page} output {base}-{page}.{ext}"
+
+def concat(recipe, fname):
+    return f"pdftk *.pdf cat output {fname}"
