18d0956fc55cb02a9f3257c82ed25ec19cbf31b7
Oleh Krehel <ohwoeowho@gmail.com>
2022-03-05 18:16:38 +0100

    pycook/recipes/clojure.py: Update

--- pycook/recipes/clojure.py
+++ pycook/recipes/clojure.py
@@ -1,5 +1,7 @@
 #* Imports
 import os
+import json
+import collections.abc
 import pycook.elisp as el
 import pycook.insta as st
 lf = el.lf
@@ -37,3 +39,24 @@ def ng_server(recipe):
 
 def ng_clojure(recipe):
     return ["ng clojure.main"]
+
+def update_in(d, u):
+    for (k, v) in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update_in(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+def outdated():
+    try:
+        j = json.loads(el.sc("cat ~/.lein/profiles.clj | jet --to json"))
+        if (j["user"]["aliases"]["outdated"] == ['run', '-m', 'antq.core'] and
+            ['com.github.liquidz/antq', '1.3.1'] in j["user"]["dependencies"]):
+            print("antq: OK")
+    except:
+        j["user"]["dependencies"].append(['com.github.liquidz/antq', '1.3.1'])
+        update_in(j, {
+            'user': {'aliases': {'outdated': ['run', '-m', 'antq.core']}}})
+        json.dump(j, open("/tmp/lein_profiles.json", "w"))
+        st.bash("cat /tmp/lein_profiles.json | jet --from json -k -p > ~/.lein/profiles.clj")
