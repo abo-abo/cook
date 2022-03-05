915ccbe813c6730386e57d5fb6004637b65bbc24
Oleh Krehel <ohwoeowho@gmail.com>
2022-03-05 18:16:21 +0100

    pycook/recipes/md.py: Update

--- pycook/recipes/md.py
+++ pycook/recipes/md.py
@@ -9,6 +9,6 @@ def export_org(recipe, fname):
     forg = re.sub("md$", "org", fname)
     return [
         (
-            "pandoc -f markdown -t org --columns=100 -o " +
+            "pandoc -f markdown -t org --wrap=preserve --columns=100 -o " +
             shlex.quote(forg) + " " + shlex.quote(fname)),
         el.emacs_batch_eval(lf('(cs-md-cleanup "{fname}")'))]
