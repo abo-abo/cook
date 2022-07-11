bdd0bd82dfab8ab64f27c3c8e6b6a17fd8eb3b3c
Oleh Krehel <ohwoeowho@gmail.com>
2022-07-11 18:29:16 +0200

    pycook/cook.py: Add "-s" flag

--- pycook/cook.py
+++ pycook/cook.py
@@ -203,17 +203,18 @@ def _main(book, module, flags, args):
             sys.stdout = sys.__stdout__
             print("\n".join([cmd[2:] for cmd in log.cmds] + ret_cmds))
         else:
-            if len(log.cmds) < 10:
-                spec = inspect.getfullargspec(fun)
-                if ("log" in spec.args) and spec.defaults and spec.defaults[spec.args.index("log") - 1] is None:
-                    pass
-                else:
-                    print("\n".join(log.cmds))
+            if "-s" not in flags:
+                if len(log.cmds) < 10:
+                    spec = inspect.getfullargspec(fun)
+                    if ("log" in spec.args) and spec.defaults and spec.defaults[spec.args.index("log") - 1] is None:
+                        pass
+                    else:
+                        print("\n".join(log.cmds))
             if ret_cmds:
                 dd = re.search("^(.*/)(cook/?)Cookbook.py$", book)
                 if dd:
                     os.chdir(dd.group(1))
-                el.bash(ret_cmds, echo=True)
+                el.bash(ret_cmds, echo=False)
 
 def modules(full=False, match=False):
     cook_dir = el.file_name_directory(recipes.__file__)
