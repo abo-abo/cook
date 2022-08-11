dc0d2b5580b51d1873345556a67578963eafc1ef
Oleh Krehel <ohwoeowho@gmail.com>
2022-08-11 18:37:52 +0200

    pycook/recipes/txt.py (to_utf8): Add

--- pycook/recipes/txt.py
+++ pycook/recipes/txt.py
@@ -1,2 +1,16 @@
+import pycook.insta as st
+import json
+
 def clean_up_term_escapes(recipe, fname):
     return f"sed -i 's/\x1b\\[[0-9;]*m//g' '{fname}'"
+
+def to_utf8(recipe, fname):
+    try:
+        j = json.loads(st.run(f"dfeal '{fname}'"))
+    except:
+        print("npm i detect-file-encoding-and-language")
+    encoding = j["encoding"]
+    if encoding == "UTF-8":
+        print("Already in UTF-8")
+    else:
+        return f"iconv -f {encoding} -t utf-8 '{fname}' | sponge '{fname}'"
