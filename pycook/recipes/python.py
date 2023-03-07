0eee3193f4d94287b576eb618743b1ed05dfffe6
Oleh Krehel <ohwoeowho@gmail.com>
2023-03-07 20:24:55 +0100

    pycook/recipes/python.py: Remove

--- pycook/recipes/python.py
+++
@@ -1,7 +0,0 @@
-def http_server(recipe):
-    return [
-        "python -m SimpleHTTPServer &",
-        "python_process=$!",
-        "sleep 0.5 && xdg-open http://localhost:8000",
-        "wait $python_process",
-    ]
